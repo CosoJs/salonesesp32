aa0fd4cd669e1d5ca68b72ceb9e4905b6258f593
CosoJs <flagrantbog@gmail.com>
2025-02-17 17:07:12 -0600

    Prueba 1 y su parte 2 anadido, creado por alma

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PruebaComponent } from './prueba/prueba.component';
+import { Parte2Component } from './parte2/parte2.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: PruebaComponent },
+  { path: 'prueba', component: PruebaComponent },
+  { path: 'parte2', component: Parte2Component },
+];
